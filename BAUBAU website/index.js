8385f920209f9f2a1bf3445ac92d94acb17f8ea0
Sohyun Kim <137376291+dioswifey@users.noreply.github.com>
2025-07-23 15:59:21 +0900

    Add files via upload

---
+++ BAUBAU website/index.js
@@ -0,0 +1,50 @@
+
+//Navbar
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+})
+//End of Navbar
+
+
+
+// Event Featuress
+
+
+const filterLinks = document.querySelectorAll(".filter-nav-link");
+
+
+function show() {
+    const showevents = document.querySelectorAll(".filter-nav");
+    showevents.style.display = 'flex';
+}
+
+
+filterLinks.forEach((filterLink) => {
+    filterLink.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        document.querySelector('.filter-nav-link').
+            classList.remove("active")
+        filterLink.classList.add("active")
+
+        const events = document.querySelectorAll(".event");
+        events.forEach((event) => {
+            event.classList.add('hide')
+
+            if (filterLink.getAttribute("data-type") ===
+                event.getAttribute("data-type")) {
+                event.classList.remove('hide');
+            }
+        })
+
+    })
+})
+// End of Event Features
+
+
